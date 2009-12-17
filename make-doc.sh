8b1247a80b76a57d05d7fd21603921f837921738
Andrew Straw <strawman@astraw.com>
2009-12-16 19:25:38 -0800

    build docs at github pages

--- make-doc.sh
+++ make-doc.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 set -e
 
-rm -rf doc
+!git status > /dev/null && { echo "aborting - repo not clean. try git status"; exit 1; }
+
+rm -rf /tmp/gh-pages
 cd doc-src
 rm -rf _build/html
 make html
-cp -a _build/html ../doc
+cp -a _build/html /tmp/gh-pages
+cd ..
+
+git checkout gh-pages
+rm -rf *
+cp -a /tmp/gh-pages/* .
+git add .
+git commit -m "update pages"
+
+git checkout master
+
