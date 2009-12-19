3b9ca8f36e6685b702519a822fd7cbc3077e7b64
Andrew Straw <strawman@astraw.com>
2009-12-19 00:27:35 -0800

    add package metadata

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from distutils.core import setup
-setup(packages=['pairs2groups'],
+setup(name='pairs2groups',
+      author='Andrew Straw',
+      url='http://astraw.github.com/pairs2groups/',
+      packages=['pairs2groups'],
       version='0.5', # also set in doc/conf.py
       license='MIT',
       )
