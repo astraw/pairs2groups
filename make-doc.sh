6fc0dbb4aff4a5bac908f505a5425847d69511a3
Andrew Straw <strawman@astraw.com>
2009-12-22 00:43:19 -0800

    fix doc-making script

--- make-doc.sh
+++ make-doc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-!git status > /dev/null && { echo "aborting - repo not clean. try git status"; exit 1; }
+git status > /dev/null && { echo "aborting - repo not clean. try git status"; exit 1; }
 
 rm -rf /tmp/gh-pages
 cd doc-src
