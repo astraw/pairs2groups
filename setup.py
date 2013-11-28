4f9f7bc2a7075b95c540d2589402a901e34d3acd
Andrew Straw <strawman@astraw.com>
2013-11-28 09:29:18 +0100

    update URL

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(name='pairs2groups',
       author='Andrew Straw',
-      url='http://astraw.github.com/pairs2groups/',
+      url='http://astraw.github.io/pairs2groups/',
       packages=['pairs2groups'],
       version='0.5', # also set in doc/conf.py
       license='MIT',
