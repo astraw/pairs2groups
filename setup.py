6b21adcc2eb04069e5e6caf9930d224f3687f04d
Andrew Straw <strawman@astraw.com>
2013-11-29 15:27:17 +0100

    add missing info for PyPI

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from distutils.core import setup
 setup(name='pairs2groups',
       author='Andrew Straw',
+      author_email='strawman@astraw.com',
       url='http://astraw.github.io/pairs2groups/',
       packages=['pairs2groups'],
       version='1.0.0', # also set in doc-src/conf.py and pairs2groups/__init__.py
       license='MIT',
+      description='compute statistical pairwise comparisons on many groups',
       )
